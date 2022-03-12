bc46ede14e9e360d7422789c105c36c9759d6035
harshitkathuria <harshitkathuria14@gmail.com>
2022-03-12 21:42:35 +0530

    Setup MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
